5e2589ab1b5694116eb97ddb69cb2753d8b3d5de
Mikiyas <127264981+miki2123j@users.noreply.github.com>
2023-11-02 20:28:45 -0400

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,16 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const Budget = () => {
+    const { expenses, Location } = useContext(AppContext);
+    const totalBudget = expenses.budget
+
+    return (
+        <div className='alert alert-primary'>
+            <span>Budget: {Location}{totalBudget}</span>
+        </div>
+    );
+};
+
+export default Budget;
 
